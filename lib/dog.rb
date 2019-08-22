fe93b4b1719ec1081ccc7c740530797a23138c00
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-08-22 19:10:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,9 +47,9 @@ class Dog
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)[0]
     # binding.pry
     if !dog
-      new_dog = self.new_from_db(dog)
-    else
       new_dog = self.create(name: name, breed: breed)
+    else
+      new_dog = self.new_from_db(dog)
     end
     new_dog
   end
